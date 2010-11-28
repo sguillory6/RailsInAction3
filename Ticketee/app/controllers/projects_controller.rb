1b5b32363f0dc097866922acc9941308896d6be1
Stanford S. Guillory <stanford.guillory@gmail.com>
2010-11-28 13:56:32 -0600

    StanG: Added the ability to view a list of all projects

--- Ticketee/app/controllers/projects_controller.rb
+++ Ticketee/app/controllers/projects_controller.rb
@@ -1,27 +1,27 @@
 class ProjectsController < ApplicationController
 
-  def index
-
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(params[:project])
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash[:alert] = "Project has not been created."
-      render :action => "new"
+    def index
+        @projects = Project.all
     end
 
-  end
+    def new
+        @project = Project.new
+    end
 
-  def show
-    @project = Project.find(params[:id])
-  end
+    def create
+        @project = Project.new(params[:project])
+        if @project.save
+            flash[:notice] = "Project has been created."
+            redirect_to @project
+        else
+            flash[:alert] = "Project has not been created."
+            render :action => "new"
+        end
+
+    end
+
+    def show
+        @project = Project.find(params[:id])
+    end
 
 end
---
+++ Ticketee/features/step_definitions/seed_steps.rb
@@ -0,0 +1,4 @@
+
+Given /^there is a project called "([^\"]*)"$/ do |name|
+  Project.create!(:name => name)
+end
